6ab75e23418cc2ad9b5e01e6f6692122e41609d3
Harish <82930803+Sivaharish12@users.noreply.github.com>
2024-02-05 19:38:54 +0530

    Add files via upload
    remark updated code

--- Controller/control.js
+++ Controller/control.js
@@ -1,5 +1,7 @@
 let users=require('../data')
 const jwt=require('jsonwebtoken')
+const decode=require('../util/decode_jwt')
+
 let count=0;
 
 exports.Signup=(req,res,next)=>{
@@ -23,7 +25,7 @@ exports.login=(req,res,next)=>{
     else throw new Error("Password Mismatch");
 }
 
-exports.token_handle=(req,res,next)=>{
+exports.refresh_token=(req,res,next)=>{
     const token=req.body.token;
     console.log(token);
     jwt.verify(token,process.env.REFRESH_TOKEN_SECRET,(err,user)=>{
@@ -39,30 +41,21 @@ exports.token_handle=(req,res,next)=>{
 
 
 
-exports.user_detail=(req,res,next)=>{
-    const auth_header=req.headers['authorization'];
-    const access_token=auth_header.split(' ')[1];
-    jwt.verify(access_token,process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
-        if(err) throw new Error("Unvlaid Access Token");
-        const decoded_access_token=jwt.decode(access_token);
-        const user_obj={id:decoded_access_token.id};
-        const logged_user=users.find(user=>user.id===user_obj.id);
-        if(logged_user==null) throw new Error("The User does not exist");
-        res.send(logged_user);
-    })
-}
+exports.user_detail = (req, res, next) => {
+    const decoded_access_token = decode.decode_jwt(req, res, next);
+    const user_obj = { id: decoded_access_token.id };
+    const logged_user = users.find(user => user.id === user_obj.id);
+    res.send(logged_user);
+};
+
 
 
 
 exports.user_detail_change=(req,res,next)=>{
     const update=req.body;
-    console.log(update);
     const keys=Object.keys(update);
-    const auth_header=req.headers['authorization'];
-    const auth_token=auth_header && auth_header.split(' ')[1];
-    const decode_auth_token=jwt.decode(auth_token);
-    const logged_user={id:decode_auth_token.id}
-    const user=users.find(user=>user.id===logged_user.id)
+    const logged_user=req.params.id;
+    const user=users.find(user=>user.id==logged_user);
     if(user==null) throw new Error("User not found");
     for(let i=0;i<keys.length;i++){
         user[keys[i]]=(update[keys[i]]);
@@ -72,10 +65,8 @@ exports.user_detail_change=(req,res,next)=>{
 
 
 exports.user_delete=(req,res,next)=>{
-    const auth_header=req.headers['authorization'];
-    const auth_token=auth_header.split(' ')[1];
-    const logged_user=jwt.decode(auth_token);
-    users=users.filter(user=>user.id!==logged_user.id);
-    res.send(`The user object id:${logged_user.id} deleted sucessfully!`);
+    const delete_id=req.params.id;
+    users=users.filter(user=>user.id!=delete_id);
+    res.send(`The user object id:${delete_id} deleted sucessfully!`);
     console.log(users);
 }
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 require('dotenv').config()
 const express=require('express')
 const app=express()
-const router=require('./Router/route');
+const user_route=require('./Router/user');
+const token_route=require('./Router/token');
 const { errors } = require('celebrate');
 
 
 app.use(express.json());
-app.use('/',router);
+app.use('/user',user_route);
+app.use('/',token_route);
+
 app.use(errors());
 app.use('/',(err,req,res,next)=>{
     if (err.name === 'CelebrationError') {
