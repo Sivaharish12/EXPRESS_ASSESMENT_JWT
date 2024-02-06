c2713b9dab478ff2bae5d9ca79af3f0b7e25a092
Harish <82930803+Sivaharish12@users.noreply.github.com>
2024-02-06 18:37:39 +0530

    Add files via upload

--- Controller/control.js
+++ Controller/control.js
@@ -1,19 +1,26 @@
-let users=require('../data')
 const jwt=require('jsonwebtoken')
 const decode=require('../util/decode_jwt')
+const db=require('../models/index')
+const user=require('../models/user');
+const { Error } = require('sequelize');
+
 
 let count=0;
 
-exports.Signup=(req,res,next)=>{
-    const user=req.body;
-    count+=1
-    user.id=count
-    users.push(user);
-    res.send(users);
+exports.Signup=async (req,res,next)=>{
+    const {name,password,confirm_password,number}=req.body;
+    try{
+        const user=await db.sequelize.models.user.create({name,password,confirm_password,number});
+        res.json(user)
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).send(err);
+    }
 }
 
-exports.login=(req,res,next)=>{
-    const user=users.find(user=>user.name===req.body.name);
+exports.login=async (req,res,next)=>{
+    const user=await db.sequelize.models.user.findOne({where :{name:req.body.name}});
     if(user==null) throw new Error("The user is empty");
     const user_obj_sign={id:user.id}
     if(user.password===req.body.password){
@@ -22,51 +29,84 @@ exports.login=(req,res,next)=>{
         console.log(user);
         res.json({access_token:access_token,refresh_tocken:refresh_tocken});
     }
-    else throw new Error("Password Mismatch");
+    else{
+        next( new Error("Password Mismatch"));
+    }
 }
 
 exports.refresh_token=(req,res,next)=>{
     const token=req.body.token;
-    console.log(token);
-    jwt.verify(token,process.env.REFRESH_TOKEN_SECRET,(err,user)=>{
-        console.log(user);
-        if(err) throw new Error("The verification failed");
-        const access_token=jwt.sign({id:user.id},process.env.ACCESS_TOKEN_SECRET,{expiresIn:"30m"});
+    const verify_token=jwt.decode(token);
+    console.log(verify_token);
+    if(verify_token){
+        const access_token=jwt.sign({id:verify_token.id},process.env.ACCESS_TOKEN_SECRET,{expiresIn:"30m"});
         const decoded_access_token=jwt.decode(access_token);
         const expirationDate = new Date(decoded_access_token.exp * 1000);
         res.send({ access_token, expirationDate });
-    });
-
+    }
+    else {
+    throw new Error("The Token is tampered");
+    }
 }
 
 
 
-exports.user_detail = (req, res, next) => {
-    const decoded_access_token = decode.decode_jwt(req, res, next);
-    const user_obj = { id: decoded_access_token.id };
-    const logged_user = users.find(user => user.id === user_obj.id);
-    res.send(logged_user);
+exports.user_detail = async (req, res, next) => {
+    try{
+        const decoded_access_token = decode.decode_jwt(req, res, next);
+        const pk=decoded_access_token.id;
+        const user=await db.sequelize.models.user.findByPk(pk);
+        res.json(user);
+    }
+    catch(err){
+        next(new Error(err));
+    }
+    
 };
 
 
 
 
-exports.user_detail_change=(req,res,next)=>{
-    const update=req.body;
-    const keys=Object.keys(update);
-    const logged_user=req.params.id;
-    const user=users.find(user=>user.id==logged_user);
-    if(user==null) throw new Error("User not found");
-    for(let i=0;i<keys.length;i++){
-        user[keys[i]]=(update[keys[i]]);
-    }
+exports.user_detail_change=async (req,res,next)=>{
+    const verify_token=decode.decode_jwt(req, res, next);
+    const pk=req.params.id;
+    if(verify_token.id==pk){
+        const update=req.body;
+        const keys=Object.keys(update)
+        const user=await db.sequelize.models.user.findByPk(pk);
+        if(user==null) next(new Error("User not found"));
+        try{
+            keys.forEach(key => {
+                user[key]=update[key];
+                user.save();
+                console.log(key);
+            });
+
+        }
+        catch(err){
+            console.error(err);
+        }
     res.send(user);
+    }
+    else{
+        next(new Error("The user does not have permission to update the record"));
+    }
 }
 
 
-exports.user_delete=(req,res,next)=>{
+exports.user_delete=async (req,res,next)=>{
+    const verify_token=decode.decode_jwt(req, res, next);
     const delete_id=req.params.id;
-    users=users.filter(user=>user.id!=delete_id);
-    res.send(`The user object id:${delete_id} deleted sucessfully!`);
-    console.log(users);
+    const user=await db.sequelize.models.user.findByPk(delete_id);
+    if(user && user.id==verify_token.id){
+        await user.destroy();
+        res.send(`The user id:${delete_id} data deleted succesfully.`);
+    }
+    else if(user){
+        next(new Error("Does not have permission"));
+    }
+    else{
+        next(new Error("User not exist"));
+    }
+   
 }
--- server.js
+++ server.js
@@ -1,50 +1,23 @@
-const express=require('express');
-const app=express();
-const db=require('./models/index');
-const user = require('./models/user');
+require('dotenv').config()
+const express=require('express')
+const app=express()
+const user_route=require('./Router/user');
+const token_route=require('./Router/token');
+const { errors } = require('celebrate');
+
+
 app.use(express.json());
+app.use('/user',user_route);
+app.use('/',token_route);
 
-app.post('/signup',async (req,res,next)=>{
-    const {name,email,password}=req.body;
-    try{
-        const user=await db.sequelize.models.user.create({name,email,password});
-        res.json(user)
+app.use(errors());
+app.use('/',(err,req,res,next)=>{
+    if (err.name === 'CelebrationError') {
+        return res.status(400).send(err.validation.message);
     }
-    catch(err){
-        console.error(err);
-        res.status(500).send(err);
-    }
+    res.status(400).send(err.message);
 })
 
-app.put('/user-update/:id',async (req,res,next)=>{
-    const pk=req.params.id;
-    const update=req.body;
-    const keys=Object.keys(update)
-    const user=await db.sequelize.models.user.findByPk(pk);
-    res.send(update)
-    try{
-        keys.forEach(key => {
-            user[key]=update[key];
-            user.save();
-            console.log(key);
-        });
 
-    }
-    catch(err){
-        console.error(err);
-    }
-})
 
-app.get('/:id',async (req,res,next)=>{
-    const pk=req.params.id;
-    const user=await db.sequelize.models.user.findByPk(pk);
-    res.json(user);
-})
-
-app.delete('/delete/:id',async (req,res,next)=>{
-    const pk=req.params.id;
-    const user=await db.sequelize.models.user.findByPk(pk);
-    await user.destroy();
-    res.send(`The user id:${pk} data deleted succesfully.`)
-})
 app.listen(5000);
